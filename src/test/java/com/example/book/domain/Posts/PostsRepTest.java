5185a15da9f42471eecdec990af24227c3cee9da
minsung gwak <131960511+opinionist@users.noreply.github.com>
2024-05-02 15:26:15 +0900

    5/2 6교시 자습시간 TEST CODE 작성

--- src/test/java/com/example/book/domain/Posts/PostsRepTest.java
+++ src/test/java/com/example/book/domain/Posts/PostsRepTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -39,5 +40,24 @@ class PostsRepTest {
         Posts post = postsList.get(0);
         assertThat(post.getTitle()).isEqualTo(title);
         assertThat(post.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BasetimeEntity(){
+        LocalDateTime now = LocalDateTime.of(2007,7,28,0,0,0,0);
+        postsRep.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        List<Posts> postsList = postsRep.findAll();
+
+        Posts post = postsList.get(0);
+
+        System.out.println(">>>>>>>>> createDate="+post.getCreatedDate()+", modifiedDate="+post.getLastModifiedDate());
+
+        assertThat(post.getCreatedDate()).isAfter(now);
+        assertThat(post.getLastModifiedDate()).isAfter(now);
     }
 }
